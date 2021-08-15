71f3292b9bc3a8c4d0e3c7392da7bd9d30b9085d
GMkonan <guilhemont@hotmail.com>
2021-08-15 03:33:10 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import tw from "twin.macro";
+
+const Home = () => {
+  return (
+    <div tw="flex flex-col items-center justify-center text-xl h-screen w-screen">
+      Simple Next + Typescript + Tailwind template
+      <p>Using twin.macro + Styled-Components too</p>
+    </div>
+  );
+};
+
+export default Home;
