3182f5c43070a15cceb97636d7fb53894a91f2fb
GMkonan <guilhemont@hotmail.com>
2021-09-07 01:02:05 -0300

    add twin config to work with plain css using twin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import tw from "twin.macro";
+import tw, { css } from "twin.macro";
 
 const Home = () => {
   return (
     <div tw="flex flex-col items-center justify-center text-xl h-screen w-screen">
       Simple Next + Typescript + Tailwind template
       <p>Using twin.macro + Styled-Components too</p>
+      <p css={[`color: #00ff00;`]}>you can style using plain css too, all using twin</p>
     </div>
   );
 };
