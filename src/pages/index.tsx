578bcc2525e2d508535924ff0681453fa70ca79e
GMkonan <guilhemont@hotmail.com>
2021-10-10 23:46:15 -0300

    Change index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
+import React from "react";
+import Hero from "src/common/components/Hero";
+import NavBar from "src/common/components/NavBar";
+import Layout from "src/layouts/Layout";
 import tw, { css } from "twin.macro";
 
 const Home = () => {
   return (
-    <div tw="flex flex-col items-center justify-center text-xl h-screen w-screen">
-      Simple Next + Typescript + Tailwind template
-      <p>Using twin.macro + Styled-Components too</p>
-      <p css={[`color: #00ff00;`]}>you can style using plain css too, all using twin</p>
-      <p>You can use styled components inside twin as well</p>
-    </div>
+    <Layout>
+      <NavBar />
+      <Hero />
+    </Layout>
   );
 };
 
